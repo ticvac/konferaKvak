e8d6573c857504f124ba2125aac9370179de5c33
ticvac <vaclav.tichy180@gmail.com>
2024-04-21 17:57:12 +0200

    start of tutorial

--- konferaKvak/settings.py
+++ konferaKvak/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "kvak.apps.KvakConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
