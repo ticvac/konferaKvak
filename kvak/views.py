a3b3b5b670003df80c5c3c5e9d788b5b98eff9f9
Jonáš Havelka <jonas.havelka@volny.cz>
2024-04-22 17:00:11 +0200

    functional view -> class-based view

--- kvak/views.py
+++ kvak/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template.response import TemplateResponse
+from django.views.generic import TemplateView
 
 
 def index(request):
@@ -11,11 +12,16 @@ def init(request):
     id = 3232321
     return HttpResponseRedirect(reverse("play", kwargs={"code":id}))
 
-def play(request, code):
-    args = {}
-    args["tiles"] = []
-    for i in range(8):
-        args["tiles"].append([])
-        for j in range(8):
-            args["tiles"][-1].append("T")
-    return TemplateResponse(request, "kvak/game.html", args) 
+class PlayView(TemplateView):
+    template_name = "kvak/game.html"
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        code = self.kwargs.get("code")
+
+        for i in range(8):
+            context_data["tiles"].append([])
+            for j in range(8):
+                context_data["tiles"][-1].append("T")
+
+        return context_data
